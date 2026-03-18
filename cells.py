ae9b5bf7659ff32092c13b9d0f6deb0e56c037b0
Asja Basovic <150077626+normala127@users.noreply.github.com>
2026-03-18 18:18:24 +0100

    "cleaned comments"

--- cells.py
+++ cells.py
@@ -17,12 +17,13 @@ class Board:
         self.columns=dimension[1]
         self.cell=Cell(0)
         self.numnberOfMines=10
+        self.createGrid()
     
     def createGrid(self):
         self.grid=np.array([[self.cell.cellState]*9 for i in range(self.rows)])
-        print("No of rows ", len(self.grid))
-        print("No of columns ", len(self.grid[0]))
-        print(self.grid[1,1])
+        #print("No of rows ", len(self.grid))
+        #print("No of columns ", len(self.grid[0]))
+        #print(self.grid[1,1])
         self.setMines()
     
     def setMines(self):
@@ -35,40 +36,40 @@ class Board:
                 #randomRow=8
                 if self.grid[randomRow,randomColumn]!=-1:
                     self.grid[randomRow,randomColumn]=-1
-                    print("Row " ,randomRow, "Column " , randomColumn)
+                    #print("Row " ,randomRow, "Column " , randomColumn)
                     break
                 
             #adjusting the hints 
             if randomRow!=0:
                 i=randomRow-1
-                print("Izabrana je opcija 1")
+                #print("Izabrana je opcija 1")
                 r=-1
             else:
                 i=randomRow
-                print("Izabrana je opcija 2")
+                #print("Izabrana je opcija 2")
                 r=0
                 
             if randomColumn!=0:
                 j=randomColumn-1
-                print("Izabrana je opcija 3")
+                #print("Izabrana je opcija 3")
                 l=-1
             else:
                 j=randomColumn
-                print("Izabrana je opcija 4")
+                #print("Izabrana je opcija 4")
                 l=0
 
             for k in range(2-r):
                 for m in range(2-l):
                     if self.grid[i,j] !=-1 and i<9:
                         self.grid[i,j] +=1
-                        print("dodaN BROJ")
+                        #print("dodaN BROJ")
                     if j==self.columns-1:
                         break
                     j+=1
                 j=randomColumn+l
-                print("------------------------")
-                self.showGrid()
-                print("------------------------")
+                #print("------------------------")
+                #self.showGrid()
+                #print("------------------------")
                 if i==self.rows-1:
                     break
                 i+=1
@@ -81,10 +82,6 @@ class Board:
 
 
 b=Board()
-b.createGrid()
-
 b.showGrid()
 
-#grid=[[0]*9 for i in range(9)]
-#for row in grid:
-#    print(row)
+# 
